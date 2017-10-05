7e6c58ef4b1e29d3b540cee8a9e617197eb0c8c4
Xochitl Fierros <xochitlfm@gmail.com>
2017-10-05 20:26:01 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,6 @@
 function basicTeenager(age) {
-
+  if (basicTeenager => 13)
+  return  "You are a teenager!"
 }
 
 function teenager(age) {
@@ -7,6 +8,7 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
+  
 
 }
 
