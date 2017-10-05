3b6d624e26e8a71d3119e70d7d5ddce22fd20ede
Xochitl Fierros <xochitlfm@gmail.com>
2017-10-05 22:01:56 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,13 @@
 function basicTeenager(age) {
   if  (age >= 13 &&  age <=19) {
-  return "You are a teenager!"
+  return "You are a teenager!";
 }
 }
 
 function teenager(age) {
-  if  (age === 13-19) {
-  else
-  {
+  if  (age >= 13 && age <= 19) {
+  return "You are a teenager!";}
+  else {
     return "You are not a teenager"
   }
 }
